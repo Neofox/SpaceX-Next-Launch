efe211f4a5531b9eacad222ae97b1e3e48a8b2ef
Jérôme Schaeffer <jer.schaeffer@gmail.com>
2019-08-09 13:45:52 +0200

    Adding a drawer

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,20 @@ import { client } from "./utils/apollo";
 import { ThemeProvider } from "@material-ui/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "./theme";
-import Navbar from "./components/Navbar";
 import EventSnackbar from "./components/EventSnackbar";
 import Dashboard from "./components/Dashboard";
 import { ApolloProvider } from "react-apollo-hooks";
+import MenuDrawer from "./components/MenuDrawer";
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <ApolloProvider client={client}>
         <CssBaseline />
-        <header>
-          <Navbar />
-        </header>
         <main>
-          <Dashboard />
+          <MenuDrawer>
+            <Dashboard />
+          </MenuDrawer>
           <EventSnackbar />
         </main>
       </ApolloProvider>
---
+++ src/components/MenuDrawer.tsx
@@ -0,0 +1,163 @@
+import React, { ReactNode } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import MenuIcon from "@material-ui/icons/Menu";
+import TimerIcon from "@material-ui/icons/Timer";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import BoatIcon from "@material-ui/icons/DirectionsBoat";
+import { makeStyles, useTheme, Theme, createStyles } from "@material-ui/core/styles";
+import githubLogo from "../github.svg";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex"
+    },
+    drawer: {
+      [theme.breakpoints.up("sm")]: {
+        width: drawerWidth,
+        flexShrink: 0
+      }
+    },
+    title: {
+      flexGrow: 1
+    },
+    appBar: {
+      marginLeft: drawerWidth,
+      [theme.breakpoints.up("sm")]: {
+        width: `calc(100% - ${drawerWidth}px)`
+      }
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up("sm")]: {
+        display: "none"
+      }
+    },
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+      width: drawerWidth
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3)
+    }
+  })
+);
+
+const MenuDrawer = (props: { children: ReactNode }) => {
+  const classes = useStyles(props);
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  function handleDrawerToggle() {
+    setMobileOpen(!mobileOpen);
+  }
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+      <Divider />
+      <List>
+        <ListItem button>
+          <ListItemIcon>
+            <TimerIcon />
+          </ListItemIcon>
+          <ListItemText primary="Next Launch" />
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem button>
+          <ListItemIcon>
+            <InboxIcon />
+          </ListItemIcon>
+          <ListItemText primary="Launches" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <BoatIcon />
+          </ListItemIcon>
+          <ListItemText primary="Ships" />
+        </ListItem>
+      </List>
+    </div>
+  );
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar variant="regular" component="header">
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" className={classes.title}>
+            SpaceX Next Launch
+          </Typography>
+          <IconButton
+            aria-label="go to the github page"
+            aria-haspopup="false"
+            onClick={() => window.open("https://github.com/Neofox/SpaceX-Next-Launch", "_blank")}
+            color="inherit"
+          >
+            <img style={{ height: 30 }} src={githubLogo} alt="github logo" />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        <Hidden smUp implementation="css">
+          <Drawer
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            ModalProps={{
+              keepMounted: true // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        {props.children}
+      </main>
+    </div>
+  );
+};
+
+export default MenuDrawer;
